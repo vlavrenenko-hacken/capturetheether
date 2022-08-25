dd2ba527277dc4fb745b2a47b09fe238965e80fa
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2022-08-25 20:59:34 +0300

    Add the source code of the GuessTheSecretNumber challenge

---
+++ src/GuessTheSecretNumber.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract GuessTheSecretNumberChallenge {
+    bytes32 private answerHash = 0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365;
+
+    constructor() payable {
+        require(msg.value == 1 ether);
+    }
+    
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function guess(uint8 n) public payable {
+        require(msg.value == 1 ether);
+
+        if (keccak256(abi.encodePacked(n)) == answerHash) {
+            (bool success,) = msg.sender.call{value: 2 ether}("");
+            require(success);
+        }
+    }
+}
